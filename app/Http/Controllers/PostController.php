24e3f9c3fcbcf65d0b1e34d593def958999fc1d5
you@example.com <you@example.com>
2025-03-05 09:38:55 +0200

    saving

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,7 +12,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        return Post::find();
+
+        return Post::all();
     }
 
     /**
@@ -20,10 +21,13 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
+        // return $request;
         $fields = $request->validate([
             'title' => 'required|max:255',
             'body' => 'required'
         ]);
+
+        $post = Post::create($fields);
 
         return $post;
     }
@@ -39,7 +43,7 @@ class PostController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Post $post)
     {
         $fields = $request->validate([
             'title' => 'required|max:255',
@@ -54,7 +58,7 @@ class PostController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy()
+    public function destroy(Post $post)
     {
         $post->delete();
         return ['message' => "The post ($post->id) has been deleted"];
--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 Route::resource('posts', PostController::class);
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::get('/', function () {
+    return 'API';
+});
